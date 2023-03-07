fbbdc1d5ca847658c07e142fc967386b31510f23
GustavoVFA <gustavovfamorim@gmail.com>
2023-03-07 20:06:49 -0300

    segundo commit

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,8 +5,8 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner grava = new Scanner(System.in);
-        int valor;
+    /* Scanner grava = new Scanner(System.in);
+       int valor;
         System.out.println("Digite um valor");
         valor = grava.nextInt();
         
@@ -15,8 +15,7 @@ public class Main {
         }else{
             System.out.println("Valor maior que dez"); 
         } 
-   
-        
+          
         int idade;
         System.out.println("Digite sua idade");
         idade = grava.nextInt();
@@ -35,8 +34,38 @@ public class Main {
         }else{
             System.out.println("Categoria Adulto");
         }
+        */
+    
+        Scanner grava = new Scanner(System.in);
         
-    }
+        int escolha; 
+        System.out.println("Digite a opção desejada \n"
+                + "[1] - Inclusão \n"
+                + "[2] - Alteração \n"
+                + "[3] - Exclusão \n"
+                + "[4] - Sair");
+        
+        escolha = grava.nextInt();
+        String nCPF, nRg, cod;
+        if(escolha == 1){
+            System.out.println("Informe CPF: ");
+            nCPF = grava.next();
+            System.out.println("Informe RG: ");
+            nRg = grava.next();
+            System.out.println("Informe Codigo: ");
+            cod = grava.next();
+            System.out.println("Cadastro realizado com sucesso");
+        }
+        else if(escolha == 2){
+            System.out.println("Registro alterado co sucesso");
+        }
+        else if(escolha == 3){
+            System.out.println("Registro Excluido co sucesso");
+        }
+        else if(escolha == 4){
+            System.out.println("Saindo do Sistema");
+        }
+    }    
 }
     
     
